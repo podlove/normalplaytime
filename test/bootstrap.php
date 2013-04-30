b847c64435aa16f332437da2ab0295356a7ea4c4
Eric Teubert <ericteubert@googlemail.com>
2013-04-30 19:00:05 +0200

    retry autoloader

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,3 @@
 <?php 
-// require 'vendor/autoload.php';
-require_once 'src/Podlove/NormalPlayTime/Parser.php';
+require 'vendor/autoload.php';
+// require_once 'src/Podlove/NormalPlayTime/Parser.php';
